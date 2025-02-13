2cdba286e7243b62af55a20d4377f7fc2ac794ac
khanbasma23 <basmakhan264@gmail.com>
2025-02-13 19:14:15 +0500

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { FiBold } from "react-icons/fi";
+
+
+const page = () => {
+  return (
+    <div>
+      <div className='bg-black w-full h-[90px] '>
+      
+        <div className=' flex justify-center  text-white text-mt-[20px] h-[20px] font-bold  '>
+          Basma khan
+          </div>
+        <FiBold className='text-blue-400 ml-[250px] border solid border-blue-400 rounded-md ' />
+        
+       
+      
+     
+        </div>
+    </div>
+  )
+}
+
+export default page
